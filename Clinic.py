69076bfda2026597c0f880e55b3cf7004254246f
CnSiva <cnsiva@localhost.localdomain>
2014-02-21 00:03:38 +0530

    Change in file structure

--- Clinic.py
+++ Clinic.py
@@ -3,40 +3,41 @@
 #coding: utf-8
 
 from defaults import *
- 
+
 @route('/')
 @route('/index.html')
 def index():
     return '<a href="/hello">Go to Hello World page</a>'
- 
+
 @route('/hello')
 def hello():
     return '<h1>HELLO WOLRD</h1>'
- 
+
 @route('/hello/:name')
 def hello_name(name):
     page = request.GET.get('page', '1')
     return '<h1>HELLO %s <br/>(%s)</h1>' % (name, page)
- 
+
 @route('/static/:filename')
 def serve_static(filename):
     return static_file(filename, root='/home/arthur/workspace/my_python_codes/src/')
- 
+
 @route('/raise_error')
 def raise_error():
     abort(404, "error...")
-route('/redirect')
+
+@route('/redirect')
 def redirect_to_hello():
     redirect('/hello')
- 
+
 @route('/ajax')
 def ajax_response():
     return {'dictionary': 'you will see ajax response right? Content-Type will be "application/json"'}
- 
+
 @error(404)
 def error404(error):
     return '404 error !!!!!'
- 
+
 @get('/upload')
 def upload_view():
     return """
@@ -45,8 +46,8 @@ def upload_view():
           <input type="file" name="data" />
           <input type="submit" name="submit" value="upload now" />
         </form>
-        """    
- 
+        """
+
 @post('/upload')
 def do_upload():
     name = request.forms.get('name')
@@ -56,7 +57,7 @@ def do_upload():
         filename = data.filename
         return "Hello %s! You uploaded %s (%d bytes)." % (name, filename, len(raw))
     return "You missed a field."
- 
+
 @route('/tpl')
 def tpl():
     return template('test')
--- defaults.py
+++ defaults.py
@@ -1,19 +1,7 @@
 
-## --------- Default Imports --------- ##
-from bottle import run
-from bottle import get
-from bottle import ext
-from bottle import post
-from bottle import error
-from bottle import abort
-from bottle import route
-from bottle import Bottle
-from bottle import request
-from bottle import redirect
-from bottle import response
-from bottle import template
-from bottle import static_file
-## --------- Default Imports --------- ##
+## --------- Bottle Imports --------- ##
+from core.bottle import *
+## --------- Bottle Imports --------- ##
 
 
 ## --------- Local Imprts ---------- ##
