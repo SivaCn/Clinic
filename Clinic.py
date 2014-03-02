b7b44b989b7d3f6b758cbba0672082fd910c292b
cnsiva <cnsiva@Linux.(none)>
2014-03-02 10:34:40 -0600

    front end support step 1

--- Clinic.py
+++ Clinic.py
@@ -32,6 +32,15 @@ def populate_backend():
             'cLzRnzbEwehP6ZzTREh3A4MXJyNo+TV8Hs4//EEbPbiDoo+dmNg22f2RJC282aSwgyWv/O6s3h42qrA6iHx8yfw=',
             '2012-10-28 20:50:26.286723'
         );
+        INSERT INTO users (username, email_addr, desc, role, hash, creation_date) VALUES
+        (
+            'user',
+            'user@localhost.local',
+            'user',
+            'user',
+            'cLzRnzbEwehP6ZzTREh3A4MXJyNo+TV8Hs4//EEbPbiDoo+dmNg22f2RJC282aSwgyWv/O6s3h42qrA6iHx8yfw=',
+            '2012-10-28 20:50:26.286723'
+        );
         INSERT INTO roles (role, level) VALUES ('special', 200);
         INSERT INTO roles (role, level) VALUES ('admin', 100);
         INSERT INTO roles (role, level) VALUES ('editor', 60);
